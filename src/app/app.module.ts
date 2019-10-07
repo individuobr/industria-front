c75730dae920ef7537f8c109007a5453f7fed07b
Sergio Costa <sergioamcosta@gmail.com>
2019-10-07 20:24:19 -0300

    criaçao dos services e estrutura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PassageiroComponent } from './passageiro/passageiro.component';
-import { BagagemComponent } from './bagagem/bagagem.component';
+import { PassageiroComponent } from './components/passageiro/passageiro.component';
+import { BagagemComponent } from './components/bagagem/bagagem.component';
 
 @NgModule({
   declarations: [
