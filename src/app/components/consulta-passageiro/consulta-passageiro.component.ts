c4e66652468664f44a4ca63340d9e82490cae8c3
Sergio Costa <sergioamcosta@gmail.com>
2019-10-18 21:55:22 -0300

    Crindo estura dos novos componentes

---
+++ src/app/components/consulta-passageiro/consulta-passageiro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-passageiro',
+  templateUrl: './consulta-passageiro.component.html',
+  styleUrls: ['./consulta-passageiro.component.css']
+})
+export class ConsultaPassageiroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
