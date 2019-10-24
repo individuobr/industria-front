44b03f22ab536a8a868949fba48a0e43b0a04cee
Sergio Costa <sergioamcosta@gmail.com>
2019-10-23 22:14:04 -0300

    Socket na tela de acompanhamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,12 @@
 import { Component } from '@angular/core';
 
-import { Message } from "@stomp/stompjs";
-import { StompState } from "@stomp/ng2-stompjs";
-import { MessageService } from './provider/services/message.service';
-
-const EXAMPLE_URL = "/topic/server-broadcaster";
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  
-  private messagingService: MessageService;
+ 
 
-  messageHistory = [];
-  state: string = "NOT CONNECTED";
-
-  constructor() {
-    // Instantiate a messagingService
-    this.messagingService = new MessageService(EXAMPLE_URL);
-
-    // Subscribe to its stream (to listen on messages)
-    this.messagingService.stream().subscribe((message: Message) => {
-      this.messageHistory.unshift(message.body);
-      console.log(message);
-    });
-
-    // Subscribe to its state (to know its connected or not)
-    this.messagingService.state().subscribe((state: StompState) => {
-      this.state = StompState[state];
-    });
-  }
-
-  // Use this methods to send message back to server
-  sendAction() {
-    console.log("Sending message");
-    this.messagingService.send("/server-receiver", {
-      text: "This is cool",
-      text2: "I'm so happy!"
-    });
-  }
 
 }
 
--- src/app/components/acompanhamento-bagagem/acompanhamento-bagagem.component.ts
+++ src/app/components/acompanhamento-bagagem/acompanhamento-bagagem.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Message } from "@stomp/stompjs";
+import { StompState } from "@stomp/ng2-stompjs";
+import { MessageService } from 'src/app/provider/services/message.service';
+
 export interface PeriodicElement {
   passageiro: string;
   poltrona: string;
@@ -34,6 +38,8 @@ const YELLOW_SECTOR: PeriodicElement[] = [
   {passageiro: 'Fabio', poltrona: '4D', bagagem: '4D'},
 ];
 
+const EXAMPLE_URL = "/topic/server-broadcaster";
+
 @Component({
   selector: 'app-acompanhamento-bagagem',
   templateUrl: './acompanhamento-bagagem.component.html',
@@ -47,10 +53,38 @@ export class AcompanhamentoBagagemComponent implements OnInit {
   dataSource_red = RED_SECTOR;
   dataSource_yellow = YELLOW_SECTOR;
 
-  constructor() { }
+  private messagingService: MessageService;
 
-  ngOnInit() {
-      
+  messageHistory = [];
+  state: string = "NOT CONNECTED";
+
+  constructor() {
+    // Instantiate a messagingService
+    this.messagingService = new MessageService(EXAMPLE_URL);
+
+    // Subscribe to its stream (to listen on messages)
+    this.messagingService.stream().subscribe((message: Message) => {
+      this.messageHistory.unshift(message.body);
+      console.log(message);
+    });
+
+    // Subscribe to its state (to know its connected or not)
+    this.messagingService.state().subscribe((state: StompState) => {
+      this.state = StompState[state];
+    });
+  }
+
+  // Use this methods to send message back to server
+  sendAction() {
+    console.log("Sending message");
+    this.messagingService.send("/server-receiver", {
+      text: "This is cool",
+      text2: "I'm so happy!"
+    });
+  }
+
+  ngOnInit(){
+    
   }
 }
 
